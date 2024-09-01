bd0f7dc756e31eb260e23c81039bda13308d61fa
ktent <zser1231@naver.com>
2024-09-02 08:39:33 +0900

    Update app.js

--- app.js
+++ app.js
@@ -44,3 +44,26 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+console.log("안녕하세요 즐거운 시작입니다.")
+app.post("/test", function(req, res) {
+  return res.send({
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          basicCard: {
+            title: "간단한 텍스트와 버튼요소",
+            description: "간단한 버튼과 텍스트입니다",
+            buttons: [
+              {
+                action: "message",
+                label: "열어보기"
+              }
+            ]
+          }
+        }
+      ]
+    }
+  });
+});
